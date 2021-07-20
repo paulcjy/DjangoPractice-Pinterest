3eee40b44cdb85a2d5bd2168430c1e736b998f13
cjy <paulcjy@naver.com>
2021-07-21 02:37:46 +0900

    Database and POST/GET

--- accountApp/models.py
+++ accountApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+	# objects = models.Manager() # objects 같은 속성들은 model 클래스에 동적으로 추가되기 때문에 vscode에서 사용하기 위해서는 이렇게 명시해두어야 한다.
+	text = models.CharField(max_length=255, null=False)
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from accountApp.models import HelloWorld
 
 # Create your views here.
 
 def helloworld(request):
-	return render(request, 'accountApp/helloworld.html')
+
+	if request.method == "POST":
+
+		data = request.POST.get('helloworld_input')
+
+		db = HelloWorld()
+		db.text = data
+		db.save()
+
+		return HttpResponseRedirect(reverse('accountApp:helloworld'))
+	else:
+		wholeData = HelloWorld.objects.all()
+
+		return render(request, 'accountApp/helloworld.html', context={'helloworld_output': wholeData})
+		# return render(request, 'accountApp/helloworld.html', context={'text': 'GET method!'})
+ 
